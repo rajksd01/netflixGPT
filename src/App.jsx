04f84be9388d10eb8d51f1424851425af7f990da
Raj Kasaudhan <rajksd9@gmail.com>
2023-09-19 16:00:21 +0530

    Initial Boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="text-5xl">Hello World</h2>
+        <p>We are going to build NETFLIXGPT</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
