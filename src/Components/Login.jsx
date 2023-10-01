edca005517066ebd1eadab35e45ee77dec98c90b
Raj Kasaudhan <rajksd9@gmail.com>
2023-10-01 17:41:06 +0530

    deployed App on firebase

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,26 +1,46 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/Validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  // to validate form
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const fullName = useRef(null);
+
+  // For Switching of Form
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
 
+  // For handling Sign up/ SignIn
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+
+    const check = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(check);
+  };
+
   return (
     <>
       <Header />
-      <div className="absolute">
+      <div className="absolute w-full h-full  ">
         {" "}
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/dc1cf82d-97c9-409f-b7c8-6ac1718946d6/14a8fe85-b6f4-4c06-8eaf-eccf3276d557/IN-en-20230911-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
+        <img
+          className="w-full h-full"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/dc1cf82d-97c9-409f-b7c8-6ac1718946d6/14a8fe85-b6f4-4c06-8eaf-eccf3276d557/IN-en-20230911-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+        />
       </div>
-      <form className=" text-white  w-4/12 absolute bg-black my-32 p-16 pt-8 mx-auto left-0 right-0 bg-opacity-80">
+      <form className=" text-white  w-4/12 absolute bg-black my-28 p-16 pt-8 mx-auto left-0 right-0 bg-opacity-80">
         <h1 className="text-3xl m-1 my-4 font-bold">
           {isSignInForm ? "SignIn" : "Sign Up"}
         </h1>
         {!isSignInForm ? (
           <input
+            ref={fullName}
             type="text"
             placeholder="Full Name"
             className="p-4  my-3 m-1 w-full bg-[#333333] rounded-md "
@@ -30,18 +50,24 @@ const Login = () => {
         )}
         <input
           type="text"
+          ref={email}
           placeholder="Email Address"
           className="p-4  my-3 m-1 w-full bg-[#333333] rounded-md "
         />
         <input
           type="password"
+          ref={password}
           placeholder="Password"
           className="p-4 m-1 my-3 w-full bg-[#333333] rounded-md"
         />
         <br />
-        <button className="bg-red-700 p-3 m-1  my-4 rounded-md w-full">
+        <button
+          className="bg-red-700 p-3 m-1  my-4 rounded-md w-full"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? <h1>Sign In</h1> : <h1>Sign Up</h1>}
         </button>
+        <p className="font-bold p-1 py-3 text-red-600">{errorMessage}</p>
         <div>
           {" "}
           <input className="m-1 " type="checkbox" />
@@ -56,3 +82,4 @@ const Login = () => {
 };
 
 export default Login;
+//1:57:43
---
+++ src/utils/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkLqUfYzXxAcgyHB-cLKvKoGyIffbrZww",
+  authDomain: "netflixgpt-0-0.firebaseapp.com",
+  projectId: "netflixgpt-0-0",
+  storageBucket: "netflixgpt-0-0.appspot.com",
+  messagingSenderId: "98093614428",
+  appId: "1:98093614428:web:ecb3172a8474b6237e2d99",
+  measurementId: "G-LB33H6ZEL9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
